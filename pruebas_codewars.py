e1528d03515365bf44e5a84745831db0460bb43a
carlgel <36209123+carlgel@users.noreply.github.com>
2018-03-06 14:35:27 -0500

    Add files via upload

---
+++ pruebas_codewars.py
@@ -0,0 +1,24 @@
+def duplicate_count(cadena):
+    # Your code goes here
+	cadena = cadena.lower()
+	caracteres = []
+	for i in range (len(cadena)):
+		repeticiones = 0
+		for j in range(len(cadena)):
+			if cadena[i] == cadena[j]:
+				print(cadena[i], cadena[j])
+				repeticiones += 1
+				variable = cadena[i] 
+		if not(variable, repeticiones) in caracteres:
+			caracteres.append((variable, repeticiones))
+	i = 0
+	while i < len(caracteres):
+		if caracteres[i][1] < 2:
+			caracteres.pop(i)
+			i = 0
+		else:
+			i += 1
+	return len(caracteres)
+# bloque principal
+
+print(duplicate_count('ABBA'))
